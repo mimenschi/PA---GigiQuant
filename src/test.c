4fe8e07bdc2d5ac4da1323cc302ec0a05f7f199c
George Cijenschi <george.cijenschi2602@gmail.com>
2026-03-25 15:05:41 +0200

    Fix: Functia de creare a listei este functionala

--- src/test.c
+++ src/test.c
@@ -10,24 +10,21 @@ struct node{
 
 typedef struct node NODE;
 
-void creereLista(int n, NODE *head, FILE *input)
+void creareLista(int n,NODE **head, FILE *input)
 {
     NODE *p, *nod, *nou;
     int i;
     double val;
-
-    
-    fseek(input, 1L*sizeof(int), 0);    //Sarit peste prima valoare din fisier
-    fseek(input, 1L*sizeof(double), 1);
     
     nod=(NODE *)malloc(sizeof(NODE));   //Creat si umplut primul element din lista
 
+    printf("%p\n",*head);
+    //fscanf(input, "%d", &n);
     fscanf(input, "%lf", &val);
     nod->valoare=val;
     nod->next=NULL;
 
-    head=nod;   
-    p=head;
+    p=(*head);
 
 
     for(i=2;i<=n;i++)
@@ -36,6 +33,8 @@ void creereLista(int n, NODE *head, FILE *input)
         fscanf(input, "%lf", &val);
         nou->valoare=val;
         nou->next=NULL;
+        
+        printf("%d: %lf, %p\n",i, nou->valoare, nou);
         p->next=nou;
         p=nou;
     }
@@ -45,7 +44,6 @@ int main(int argc, char *argv[])
 {
     FILE *input, *output;
 
-    int nrObserv; //numarul de observatii (prima linie a fisierului)
 
     input=fopen(argv[1], "rb");
     output=fopen(argv[2],"wb");
@@ -61,23 +59,32 @@ int main(int argc, char *argv[])
         exit(2);
     }
 
-    fscanf(input, "%d", &nrObserv);
-
     NODE *head, *nod;
     double val;
+    int n;
+    head=(NODE *)malloc(sizeof(NODE));
     nod=(NODE *)malloc(sizeof(NODE));
+    fscanf(input, "%d", &n);
+    printf("n=%d\n",n);
     fscanf(input, "%lf", &val);
     nod->valoare=val;
     nod->next=NULL;
     head=nod;
 
-    creereLista(nrObserv, head, input);
+    creareLista(n, &head, input);
 
     NODE *p;
+    p=(NODE *)malloc(sizeof(NODE));
     int i=1;
+
+    printf("aici\n");
+    printf("%p",head);
+    printf("%p",head->next);
+
 
     for(p=head; p->next != NULL; p=p->next)
     {
+        printf("\naici 2 %p ", p);
         printf("Valoarea nodului %d: %lf",i,p->valoare);
         i++;
     }
