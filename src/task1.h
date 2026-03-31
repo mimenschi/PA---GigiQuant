65b1c1e1c4be942aecfafb4ff358b0857838fbc9
George Cijenschi <george.cijenschi2602@gmail.com>
2026-03-31 22:04:16 +0300

    Rebrand: Creat fisier cu functii si fisier header

---
+++ src/task1.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct node{
+    double valoare;
+    double randament;
+    struct node *next;
+};
+
+typedef struct node NODE;
+
+void creareLista(int n,NODE **head, FILE *input);
+void stergereLista(NODE **head);
+
+double calculRand(double p1, double p2);
+void adaugareRand(NODE *head);
+double randamentMediu(int n, NODE *head);
+double calculVolatilitate(int n, double ranMed, NODE *head);
+double calculSharpeRaio(double randMed, double volatilitate);
--- src/test.c
+++ src/test.c
@@ -1,112 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-struct node{
-    double valoare;
-    double randament;
-    struct node *next;
-};
-
-typedef struct node NODE;
-
-void creareLista(int n,NODE **head, FILE *input)
-{
-    NODE *p, *nou;
-    int i;
-    double val;
-
-    p=(*head);
-
-
-    for(i=2;i<=n;i++)
-    {
-        nou=(NODE *)malloc(sizeof(NODE));
-        fscanf(input, "%lf", &val);
-        nou->valoare=val;
-        nou->next=NULL;
-        
-        p->next=nou;
-        p=nou;
-
-        //printf("s a creat nodul %d la %p se uita la %p: %lf\n",i, p, p->next, p->valoare); //Pentru test lista
-    }
-    //printf("Ultimul ultimul nod are adresa: %p\n", p);
-    //printf("In functie: ultimul next: %p\n",p->next);
-}
-
-void stergereLista(NODE **head)
-{
-    NODE *p;
-
-    while((*head)!=NULL)
-    {
-        p=(*head)->next;
-        free(*head);
-        *head = p;
-    }
-    //free(p);
-    *head=NULL;
-}
-
-double calculRand(double p1, double p2)
-{
-    return ( (p2-p1)/p1 )*1.0;
-}
-
-void adaugareRand(NODE *head)
-{
-    NODE *p;
-
-    for(p=head; p->next != NULL; p=p->next)
-    {
-        (p->next)->randament=calculRand(p->valoare, (p->next)->valoare );
-    }
-}
-
-double randamentMediu(int n, NODE *head)
-{
-        double suma=0, randMed=0;
-        NODE *p;
-
-        for(p=head->next; p!=NULL; p=p->next)
-        {
-            suma += p->randament;
-        }
-        
-        randMed = (1.0/(n-1) )*suma;
-
-        return randMed;
-}
-
-double calculVolatilitate(int n, double ranMed, NODE *head)
-{
-    double volatilitate=0, suma=0;
-    NODE *p;
-    for(p=head->next; p!=NULL; p=p->next)
-    {
-        suma += pow(p->randament - ranMed, 2); 
-    }
-
-    volatilitate = sqrt( (1.0)/(n-1) * suma );
-    return volatilitate;
-}
-
-double calculSharpeRaio(double randMed, double volatilitate)
-{
-    double sharpeR=0;
-    sharpeR = (1.0)*randMed / volatilitate;
-    return sharpeR;
-}
+#include "task1.h"
 
 int main(int argc, const char *argv[])
 {
     FILE *input, *output;
 
-
     input=fopen(argv[1], "rb");
     output=fopen(argv[2],"wb");
 
+    //Verificare status fisiere
     if(input == NULL)
     {
         printf("Eroare, fisierul de input nu a putut fi deschis.\n");
@@ -118,6 +22,7 @@ int main(int argc, const char *argv[])
         exit(2);
     }
 
+    //Head ul listei
     NODE *head;
     double val;
     int n;
@@ -161,6 +66,7 @@ int main(int argc, const char *argv[])
     volatilitate = calculVolatilitate(n, randMed, head);
     sharpeR = calculSharpeRaio(randMed, volatilitate);
 
+    //Trunchierea valorilor calculate
     randMed=((int)(randMed*1000))*1.0/1000;
     volatilitate=((int)(volatilitate*1000))*1.0/1000;
     sharpeR=((int)(sharpeR*1000))*1.0/1000;
@@ -171,11 +77,15 @@ int main(int argc, const char *argv[])
     printf("\nShrapeR este: %.3lf\n", sharpeR);              ///
     */
 
+    //Printarea valorilor in fisierul de output
     fprintf(output, "%.3lf\n", randMed);
     fprintf(output, "%.3lf\n", volatilitate);
     fprintf(output, "%.3lf\n", sharpeR);
 
+    //Golirea listei
     stergereLista(&head);
+
+    //Inchiderea fisierleor
     fclose(input);
     fclose(output);
     return 0;
